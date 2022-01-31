134d7c06c5b2276c34a14fde65e88bedb0b25150
Reekwind <98742722+Reekwind@users.noreply.github.com>
2022-01-31 14:01:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace MyMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = "This restaurant was wonderful."
+            };
+
+            // Load model and predict output of sample data
+            var result = SentimentModel.Predict(sampleData);
+
+            // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+            string sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+        }
+    }
+}
